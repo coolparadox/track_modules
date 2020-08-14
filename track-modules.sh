60144cb3a90be36128509f4b9ce63de1943dd034
Luiz Fernando A. Silva <luiz-fernando@LF.Luiz>
2020-08-14 19:51:29 -0300

    First test 2.0

--- track-modules.sh
+++ track-modules.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 deps(){
-test -n "$1" || return
+	test -n "$1" || return
 	n=$1
 	echo $n
-/usr/sbin/modinfo -k 5.4.48-gentoo-tormenta16 --basedir sample --field depends $n | tr "," "\n" | \
-while read l; do
-	deps $l 
-done
+	/usr/sbin/modinfo -k 5.4.48-gentoo-tormenta16 --basedir sample --field depends $n | tr "," "\n" | \
+	while read l; do
+		deps $l
+	done
 }
 
-deps i915
+deps $1
