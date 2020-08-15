f41571d7193716cfa00df90ab0e4a7a6cc269d90
Luiz Fernando A. Silva <luiz-fernando@LF.Luiz>
2020-08-15 18:50:25 -0300

    Fix order
    fixes #2
    fixes #5

--- track-modules.sh
+++ track-modules.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+F='/tmp/mod.txt'
+rm -f $F
+touch $F
+no-reps(){
+	while read m; do
+		fgrep -q $m $F || {
+			echo $m >> $F
+			echo $m
+		}
+	done
+}
 
 deps(){
 	test -n "$1" || return
-	n=$1
+	n=$@
 	/usr/sbin/modinfo -k 5.4.48-gentoo-tormenta16 --basedir sample --field depends $n | tr "," "\n" | \
 	while read l; do
 		deps $l
 	done
-	echo $n
+ 	echo $n
 }
 
-deps $1
+deps $@ | no-reps
