ee16e3cf837a49643d49d91e985f3de7434090ac
Luiz Fernando A. Silva <luiz-fernando@LF.Luiz>
2020-08-14 22:31:26 -0300

    Fix order
    fixes #2

--- track-modules.sh
+++ track-modules.sh
@@ -3,11 +3,11 @@
 deps(){
 	test -n "$1" || return
 	n=$1
-	echo $n
 	/usr/sbin/modinfo -k 5.4.48-gentoo-tormenta16 --basedir sample --field depends $n | tr "," "\n" | \
 	while read l; do
 		deps $l
 	done
+	echo $n
 }
 
 deps $1
